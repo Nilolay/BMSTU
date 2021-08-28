b3927c086e6d064a6b1985b057ee54b0b84ece8a
Николай Рудич <sadako9000@gmail.com>
2021-08-28 10:51:29 +0300

    Mobile project

---
+++ Phoneword1/Phoneword1.iOS/PhoneDialer.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using Phoneword1.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword1.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+            new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ Phoneword1/Phoneword1/Interface1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoneword1
+{
+    public interface IDialer
+    {
+        bool Dial(string number);
+    }
+}
